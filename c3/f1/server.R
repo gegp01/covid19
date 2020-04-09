dfce90bafd23ee55ed4925f6ebfeb2a33f3a3596
Gabriel Ernesto García Peña <5376741+gegp01@users.noreply.github.com>
2020-04-09 07:38:07 -0500

    Update server.R

--- c3/f1/server.R
+++ c3/f1/server.R
@@ -9,26 +9,26 @@ server <- function(input, output) {
     output$log_plot <- renderPlot({
         
         par(bg="grey30", col.lab="white", col.main="white", col.axis="white", lwd=1.5, cex=1.5)
-        plot(spline(log(mex_covid19[,1]+1)~dia), type="l", col="red"
+        plot(spline(log10(mex_covid19[,1]+1)~dia), type="l", col="red"
              , axes=F
              , xlab="días desde el 20 de enero de 2020"
-             , ylab="ln casos"
-             , ylim=c(0,15))
+             , ylab="log(10) casos"
+             , ylim=c(0,6))
         
-        lines(spline(log(mex_covid19[,2]+1)~dia), type="l", col="red", lty="dotdash")            
-        lines(spline(log(usa_covid19[,1]+1)~dia), type="l", col="lightblue")
-        lines(spline(log(usa_covid19[,2]+1)~dia), type="l", col="lightblue", lty="dotdash")
-        lines(spline(log(italia_covid19[,1]+1)~dia), type="l", col="green")
-        lines(spline(log(italia_covid19[,2]+1)~dia), type="l", col="green", lty="dotdash")
-        lines(spline(log(spain_covid19[,1]+1)~dia), type="l", col="gold")
-        lines(spline(log(spain_covid19[,2]+1)~dia), type="l", col="gold", lty="dotdash")
+        lines(spline(log10(mex_covid19[,2]+1)~dia), type="l", col="red", lty="dotdash")            
+        lines(spline(log10(usa_covid19[,1]+1)~dia), type="l", col="lightblue")
+        lines(spline(log10(usa_covid19[,2]+1)~dia), type="l", col="lightblue", lty="dotdash")
+        lines(spline(log10(italia_covid19[,1]+1)~dia), type="l", col="green")
+        lines(spline(log10(italia_covid19[,2]+1)~dia), type="l", col="green", lty="dotdash")
+        lines(spline(log10(spain_covid19[,1]+1)~dia), type="l", col="gold")
+        lines(spline(log10(spain_covid19[,2]+1)~dia), type="l", col="gold", lty="dotdash")
         
         axis(1, col="white")
         axis(2, col="white")
         grid(col="ivory3")
         title(paste("Reporte de la OMS:", tail(who$Date, 1)))
         
-        legend(1, 14, legend=c("MX", "US", "IT", "ES", "confirmados", "muertos"),
+        legend(1, 5.5, legend=c("MX", "US", "IT", "ES", "confirmados", "muertos"),
                col=c("red", "lightblue", "green", "gold", "black", "black")
                , lty=c(rep(1, 4), 1, 2), cex=0.8, bg="beige")
         
