c2c2cc7b774ef0ec744e690713fb4d6b055479b7
Gabriel Ernesto García Peña <5376741+gegp01@users.noreply.github.com>
2020-04-17 00:58:34 -0500

    Create wget_covid19MX.R

---
+++ c3/wget_covid19MX.R
@@ -0,0 +1,18 @@
+# IMPLEMENTED FROM: https://hydroecology.net/downloading-extracting-and-reading-files-in-r/
+# create a temporary directory
+td = tempdir()
+# create the placeholder file
+tf = tempfile(tmpdir=td, fileext=".zip")
+# download into the placeholder file
+download.file("http://187.191.75.115/gobmx/salud/datos_abiertos/datos_abiertos_covid19.zip", tf)
+
+# get the name of the first file in the zip archive
+fname = unzip(tf, list=TRUE)$Name[1]
+# unzip the file to the temporary directory
+unzip(tf, files=fname, exdir=td, overwrite=TRUE)
+# fpath is the full path to the extracted file
+fpath = file.path(td, fname)
+
+# stringsAsFactors=TRUE will screw up conversion to numeric!
+d = read.csv(fpath, header=TRUE, row.names=NULL, 
+             stringsAsFactors=FALSE)
