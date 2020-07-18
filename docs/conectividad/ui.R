3ecc4f1bf9f3610432bf3d60a595b7c8584b8ee2
Gabriel Ernesto García Peña <5376741+gegp01@users.noreply.github.com>
2020-07-18 06:54:03 -0500

    Create ui.R

---
+++ docs/conectividad/ui.R
@@ -0,0 +1,23 @@
+# author: Gabriel Ernesto García-Peña
+# beta version @
+# https://gegp01.shinyapps.io/conectividad_municipios
+library(shiny)
+library(shinythemes)
+library(shinyjs)
+library(DT)
+library(visNetwork)
+library(epicontacts)
+library(adegenet)
+
+options(shiny.maxRequestSize = 30*1024^2) # ALLOW HANDLING LARGE FILES
+
+ui <- fluidPage(
+    tags$style('.container-fluid {
+                             background-color:#FFFFFF;}')  #f7f9f9
+    , htmlOutput("text3")
+    , selectInput("origen1","", choices = NULL, width="90%")
+    , visNetworkOutput('d3f')
+    , htmlOutput("text1")
+    , htmlOutput("text2")
+    , dataTableOutput("links")
+        )
